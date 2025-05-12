79b5490e4ce355d643062056380dd624b12a7a99
Nicolas Sursock <nsursock@Mac.lan>
2025-05-12 12:41:52 +0300

    Fixed loading problems of notyf

--- src/assets/js/stores/notifications.js
+++ src/assets/js/stores/notifications.js
@@ -1,3 +1,5 @@
+import { Notyf } from 'notyf';
+
 // Create a store for notifications
 export default {
     notyf: null,
@@ -24,6 +26,54 @@ export default {
                             className: 'icon-[tabler--circle-x]',
                             tagName: 'i'
                         }
+                    },
+                    {
+                        type: 'info',
+                        background: 'var(--color-info)',
+                        icon: {
+                            className: 'icon-[tabler--info-circle]',
+                            tagName: 'i'
+                        }
+                    },
+                    {
+                        type: 'warning',
+                        background: 'var(--color-warning)',
+                        icon: {
+                            className: 'icon-[tabler--alert-triangle]',
+                            tagName: 'i'
+                        }
+                    },
+                    {
+                        type: 'primary',
+                        background: 'var(--color-primary)',
+                        icon: {
+                            className: 'icon-[tabler--star]',
+                            tagName: 'i'
+                        }
+                    },
+                    {
+                        type: 'secondary',
+                        background: 'var(--color-secondary)',
+                        icon: {
+                            className: 'icon-[tabler--star-half]',
+                            tagName: 'i'
+                        }
+                    },
+                    {
+                        type: 'accent',
+                        background: 'var(--color-accent)',
+                        icon: {
+                            className: 'icon-[tabler--sparkles]',
+                            tagName: 'i'
+                        }
+                    },
+                    {
+                        type: 'neutral',
+                        background: 'var(--color-neutral)',
+                        icon: {
+                            className: 'icon-[tabler--circle]',
+                            tagName: 'i'
+                        }
                     }
                 ]
             });
@@ -38,5 +88,53 @@ export default {
     error(message) {
         this.init();
         this.notyf.error(message);
+    },
+
+    info(message) {
+        this.init();
+        this.notyf.open({
+            type: 'info',
+            message: message
+        });
+    },
+
+    warning(message) {
+        this.init();
+        this.notyf.open({
+            type: 'warning',
+            message: message
+        });
+    },
+
+    primary(message) {
+        this.init();
+        this.notyf.open({
+            type: 'primary',
+            message: message
+        });
+    },
+
+    secondary(message) {
+        this.init();
+        this.notyf.open({
+            type: 'secondary',
+            message: message
+        });
+    },
+
+    accent(message) {
+        this.init();
+        this.notyf.open({
+            type: 'accent',
+            message: message
+        });
+    },
+
+    neutral(message) {
+        this.init();
+        this.notyf.open({
+            type: 'neutral',
+            message: message
+        });
     }
 }; 
